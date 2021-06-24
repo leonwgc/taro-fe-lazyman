edb3ffc397a38badbdd2c77b860409b227f4dde9
leonwgc <giantfish@126.com>
2021-06-24 10:39:24 +0800

    feat: init

---
+++ src/app.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import { Provider, configureStore } from 'simple-redux-store';
+import './app.less';
+
+const store = configureStore({ name: 'wgc', counter: 0 });
+
+class App extends Component {
+  componentDidMount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  componentDidCatchError() {}
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render() {
+    return <Provider store={store}>{this.props.children}</Provider>;
+  }
+}
+
+export default App;
